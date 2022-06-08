a3df85fc35f52bb28b1877cba7fb25f8760d48e1
Tejas Chuahan <developertejas@gmail.com>
2022-06-08 20:31:35 +0530

    Update index.php

--- custom-filter-tejas/index.php
+++ custom-filter-tejas/index.php
@@ -1,11 +1,11 @@
 <?PHP 
 /* 
 Plugin Name: Custom Filter by Tejas
-Plugin URI: hikebranding.com
+Plugin URI: https://www.linkedin.com/in/tejas-chauhan-77987480/
 Description: Custom filter for woocommerce products
 Version: 1
 Author: Tejas
-Author URI: hikebranding.com
+Author URI: https://www.linkedin.com/in/tejas-chauhan-77987480/
 License: GPLv2 or later
 */	
 
